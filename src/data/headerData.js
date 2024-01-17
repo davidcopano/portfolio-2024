c4c1f1bdfb8e1a8c6d8f41d8641385fa8bd79e93
David Copano <davidcopano96@gmail.com>
2024-01-17 19:20:07 +0100

    modificando datos personales

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,9 +1,9 @@
 import resume from '../assets/pdf/resume.pdf'
 
 export const headerData = {
-    name: 'Jane Doe',
-    title: "Web Developer",
-    desciption:"There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
+    name: 'David Copano',
+    title: 'Soy',
+    desciption: "Desarrollador web y de aplicaciones móviles híbridas en búsqueda de proyectos emocionantes. Experto en transformar código en soluciones prácticas y atractivas. ¡Hablemos! 💻📱🚀",
     image: 'https://raw.githubusercontent.com/PhantomScript/asset-container/main/developer-portfolio/landingImg.png',
     resumePdf: resume
 }
