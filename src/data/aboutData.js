e146cedfdb95980dbbfbabd4cd05dc5bae503e30
David Copano <davidcopano96@gmail.com>
2024-01-17 19:49:10 +0100

    modificando datos personales

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
-    title: "Who I am",
-    description1: "My name's Jane. I'm a web designer and developer based in Southampton, UK.",
-    description2: "During the day I work as a lead developer at a local agency and in the evening I  work on freelance projects and utilize the time to built my own products. I spent my leisure hours writing articles and poetry. Right now I'm also trying a hand at machine learning and AI. I love to learn and explore new arenas.",
-    image: 2
+    title: "Sobre mí",
+    description1: "Me llamo David Copano Jiménez. Soy desarrollador web y de aplicaciones móviles híbridas de Jerez de la Frontera, España",
+    description2: "Trabajo como desarrollador en mi empresa, mi tiempo libre suelo emplearlo en formarme en nuevas tecnologías, pasar tiempo con los míos... Me encanta aprender y explorar nuevas tecnologías",
+    image: 1
 }
--- src/data/socialsData.js
+++ src/data/socialsData.js
@@ -1,14 +1,5 @@
 export const socialsData = {
-    github: 'https://github.com/',
-    facebook: 'https://www.facebook.com/',
-    linkedIn: 'https://www.linkedin.com/in',
-    instagram: 'https://www.instagram.com/',
-    codepen: 'https://codepen.io/',
-    twitter: 'https://twitter.com/',
-    reddit: 'https://www.reddit.com/user/',
-    blogger: 'https://www.blogger.com/',
-    medium: 'https://medium.com/@',
-    stackOverflow: 'https://stackoverflow.com/users/',
-    gitlab: 'https://gitlab.com/',
-    youtube: 'https://youtube.com/'
+    github: 'https://github.com/davidcopano',
+    linkedIn: 'https://www.linkedin.com/in/david-jes%C3%BAs-copano-jim%C3%A9nez-16b1a1121/',
+    instagram: 'https://www.instagram.com/davidcopano'
 }
