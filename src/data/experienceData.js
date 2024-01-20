095a27344177d1d110164e143dbb07a7ed3b0790
David Copano <davidcopano96@gmail.com>
2024-01-20 13:32:33 +0100

    modificando datos personales

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,23 +1,16 @@
 export const experienceData = [
     {
         id: 1,
-        company: 'Globex Corporation',
-        jobtitle: 'Frontend Developer',
-        startYear: '2018',
-        endYear: '2019'
+        company: 'Xerintel Internet Technologies SL',
+        jobtitle: 'Desarrollador web y aplicaciones móviles híbridas',
+        startYear: 'Septiembre 2017',
+        endYear: 'Presente'
     },
     {
         id: 2,
-        company: 'Vehement Capital Partners',
-        jobtitle: 'Backend Developer',
-        startYear: '2019',
-        endYear: '2020'
-    },
-    {
-        id: 3,
-        company: 'Wonka Industries',
-        jobtitle: 'Marketing',
-        startYear: '2020',
-        endYear: 'Present'
+        company: 'Trendix - Creative Apps',
+        jobtitle: 'Desarrollador web',
+        startYear: 'Marzo 2017',
+        endYear: 'Junio 2017'
     },
 ]
