1eac88930ea34bbafafb9235baa5d7037cb72f51
David Copano <davidcopano96@gmail.com>
2024-01-20 13:25:28 +0100

    modificando datos personales

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -1,23 +1,16 @@
 export const educationData = [
     {
         id: 1,
-        institution: 'Marble Hills Middle School',
-        course: 'Higher Secondary Education',
-        startYear: '2007',
-        endYear: '2015'
+        institution: 'IES Francisco Romero Vargas, Cádiz',
+        course: 'CFGS - Desarrollo de Aplicaciones Web',
+        startYear: '2014',
+        endYear: '2017'
     },
     {
         id: 2,
-        institution: 'Somerset School of Science',
-        course: 'Bachelor of Technology',
-        startYear: '2015',
-        endYear: '2019'
-    },
-    {
-        id: 3,
-        institution: 'Hawking University, Cambridge',
-        course: 'Master of Technology',
-        startYear: '2019',
-        endYear: 'Present'
-    },
+        institution: 'IES Asta Regia, Cádiz',
+        course: 'ESO y Bachillerato de Ciencias',
+        startYear: '2008',
+        endYear: '2014'
+    }
 ]
--- src/data/skillsData.js
+++ src/data/skillsData.js
@@ -1,13 +1,17 @@
  export const skillsData = [
     'HTML',
-    'Fastify',
-    'Blender',
-    'Figma',
-    'Javascript',
     'CSS',
-    'React',
-    'Django',
-]
+    'Javascript',
+    'PHP',
+    'MySQL',
+    'Bootstrap',
+    'Angular',
+    'Ionic',
+    'CapacitorJs',
+    'Typescript',
+    'Git',
+    'Markdown',
+];
 
 // Choose your skills from below. Make sure it's in the same format and spelled correctly.
 // Couldn't find the required skills? Raise an issue on github at https://github.com/hhhrrrttt222111/developer-portfolio/issues/new
