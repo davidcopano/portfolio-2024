a657b9419457c6e397d27150ccb8c969d24e5e0c
David Copano <davidcopano96@gmail.com>
2025-02-07 14:30:13 +0100

    📝 docs(index.html): actualizar enlaces y metadatos del portafolio
    - cambiar la URL de la imagen de la propiedad og:image
    - actualizar la URL a la que apuntan las propiedades og:url y itemprop:url
    - modificar la URL de twitter:url para reflejar el nuevo dominio
    
    📝 docs(headerData.js): actualizar imagen en los datos del encabezado
    
    - cambiar la URL de la imagen en el objeto headerData

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,6 +4,6 @@ export const headerData = {
     name: 'David Copano',
     title: 'Soy',
     description: "Desarrollador web y de aplicaciones móviles híbridas en búsqueda de proyectos emocionantes. Experto en transformar código en soluciones prácticas y atractivas. ¡Hablemos! 💻📱🚀",
-    image: 'https://i.imgur.com/iAVHICA.jpg',
+    image: 'https://i.imgur.com/8JUho2j.jpeg',
     resumePdf: resume
 }
